e760fcccf4a197480f4d690ca3326ce5539f2637
Rahim Ghasemi <43703064+hampadco@users.noreply.github.com>
2024-07-16 02:27:09 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,13 +13,11 @@ rest='\033[0m'
 install_packages() {
     local packages=(curl jq bc)
     local missing_packages=()
-
     for pkg in "${packages[@]}"; do
         if ! command -v "$pkg" &> /dev/null; then
             missing_packages+=("$pkg")
         fi
     done
-
     if [ ${#missing_packages[@]} -gt 0 ]; then
         if [ -n "$(command -v pkg)" ]; then
             pkg install "${missing_packages[@]}" -y
@@ -72,7 +70,7 @@ select_card() {
 
 # Function to get the best card
 get_best_card() {
-    echo "$1" | jq -r '.results[] | select(.status == "STARTED") | .id as $id | .effect_function.params | {id: $id, ratio: (.dst_amount / .coin_amount)}' | jq -s 'sort_by(-.ratio)[0]'
+    echo "$1" | jq -r '.results[] | select(.status == "STARTED" and .requirement.cards == []) | .id as $id | .effect_function.params | {id: $id, ratio: (.dst_amount / .coin_amount)}' | jq -s 'sort_by(-.ratio)[0]'
 }
 
 # Main script logic
@@ -80,33 +78,35 @@ main() {
     while true; do
         # Get the list of cards
         card_list=$(get_card_list)
-
+        
         # Get the best card
         best_card=$(get_best_card "$card_list")
-
-        if [ -z "$best_card" ]; then
+        
+        if [ -z "$best_card" ] || [ "$best_card" == "null" ]; then
             echo -e "${yellow}No suitable card found. Waiting for 60 seconds before trying again...${rest}"
             sleep 60
             continue
         fi
-
+        
         card_id=$(echo "$best_card" | jq -r '.id')
         ratio=$(echo "$best_card" | jq -r '.ratio')
-
+        
         echo -e "${purple}============================${rest}"
         echo -e "${green}Best card to select:${yellow} $card_id${rest}"
         echo -e "${blue}Profit/Cost Ratio: ${cyan}$ratio${rest}"
         echo ""
-
+        
         echo -e "${green}Attempting to select card '${yellow}$card_id${green}'...${rest}"
         selection_result=$(select_card "$card_id")
-
-        if echo "$selection_result" | jq -e '.id' > /dev/null; then
+        
+        if [ -z "$selection_result" ] || [ "$selection_result" == "null" ]; then
+            echo -e "${red}Failed to select card ${yellow}'$card_id'${red}. Null response received.${rest}"
+        elif echo "$selection_result" | jq -e '.id' > /dev/null; then
             echo -e "${green}Card ${yellow}'$card_id'${green} selected successfully.${rest}"
         else
-            echo -e "${red}Failed to select card ${yellow}'$card_id'${red}. Error: ${cyan}$(echo "$selection_result" | jq -r '.detail')${rest}"
+            echo -e "${red}Failed to select card ${yellow}'$card_id'${red}. Error: ${cyan}$(echo "$selection_result" | jq -r '.detail // "Unknown error"')${rest}"
         fi
-
+        
         echo -e "${green}Waiting for 10 seconds before next selection...${rest}"
         sleep 10
     done
